19ccc61243e39d7ef5d003b912ef311b7b372636
Izy <126877709+izy-code@users.noreply.github.com>
2023-07-21 20:19:31 +0300

    feat: Hide scroll bar while modal window showed

--- scripts/modal-window.js
+++ scripts/modal-window.js
@@ -5,14 +5,20 @@ const dialogSearch = document.querySelector('.index__modal-search');
 function connectButtonsWithDialog(buttonOpen, dialog) {
   buttonOpen.addEventListener('click', () => {
     dialog.showModal();
+    // Скрытие полосы прокрутки основного окна
+    document.body.style.overflow = "hidden";
     // Кнопка закрытия окна
     const buttonClose = dialog.querySelector('.modal-close-button');
     buttonClose.onclick = () => {
       dialog.close();
+      document.body.style.overflow = "visible";
     };
-    // Закрытие модального окна после клика на backdrop
+    // Закрытие модального окна после клика на backdrop и возврат полосы прокрутки
     dialog.onclick = (event) => {
-      if (event.target === dialog) dialog.close();
+      if (event.target === dialog) {
+        dialog.close();
+        document.body.style.overflow = "visible";
+      }
     };
   });
 }
